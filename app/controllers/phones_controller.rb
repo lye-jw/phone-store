02a8f19e9f69ac57a9a2a4e160290005ea3578c0
lye-jw <lyejianwen@gmail.com>
2020-02-12 21:42:04 +0800

    Show error message when updating or adding entry fails

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,48 +1,50 @@
 class PhonesController < ApplicationController
-    def index
-        @phones = Phone.all
-    end
+  def index
+    @phones = Phone.all.order('id')
+  end
 
-    def new
-        @phone = Phone.new
-    end
+  def new
+    @phone = Phone.new
+  end
 
-    def create
-        @phone = Phone.new(phone_params)
-        if @phone.save
-          redirect_to phones_path
-        else
-          redirect_to new_phone_path
-        end
-     end
+  def create
+    @phone = Phone.new(phone_params)
+    if @phone.save
+      redirect_to phones_path
+    else
+      flash[:alert] = @phone.errors.full_messages.to_sentence
+      render 'new'
+    end
+  end
     
-    def show
-        @phone = Phone.find(params[:id])
-    end
+  def show
+    @phone = Phone.find(params[:id])
+  end
 
-    def edit
-        @phone = Phone.find(params[:id])
-    end
+  def edit
+    @phone = Phone.find(params[:id])
+  end
 
-    def update
-        @phone = Phone.find(params[:id])
-        if @phone.update(phone_params)
-          redirect_to phones_path
-        else
-          redirect_to edit_phone_path
-      end
+  def update
+    @phone = Phone.find(params[:id])
+    if @phone.update(phone_params)
+      redirect_to phones_path
+    else
+      flash[:alert] = @phone.errors.full_messages.to_sentence
+      redirect_to edit_phone_path
     end
+  end
 
-    def destroy
-        @phone = Phone.find(params[:id])
-        @phone.destroy
-          redirect_to phones_path
-        end
-      end
+  def destroy
+    @phone = Phone.find(params[:id])
+    @phone.destroy
+      redirect_to phones_path
+    end
+  end
 
       
-    private
+  private
 
-    def phone_params
-        params.require(:phone).permit(:name, :price, :quantity, :brand_id)
-    end
+  def phone_params
+    params.require(:phone).permit(:name, :price, :quantity, :brand_id)
+  end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,4 +1,5 @@
 class Phone < ApplicationRecord
+    validates :price, numericality: { only_integer: true }
     validates :quantity, numericality: {greater_than_or_equal_to: 0}
     belongs_to :brand, optional: true
 end
